16564c79120f71a9f00ba859fca0e79b4a7b85aa
mahmoodhussam <mahmoodhussam@gmail.com>
2021-01-18 20:42:10 +0200

    Simple change

--- script.js
+++ script.js
@@ -11,9 +11,9 @@ btn.addEventListener('click',function(){
 			success:function(data,status){
 				document.getElementById("city-name").textContent = `${data.name} ,${data.sys.country}`;				
 				document.getElementById('icon').src=`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
-				document.getElementById("city-temp").innerHTML=`${Math.round(data.main.temp)}<sup id="circle">&#9898;</sup>c`;
+				document.getElementById("city-temp").innerHTML=`${Math.round(data.main.temp)}<sup id="circle">o</sup>c`;
 				document.getElementById("city-status").textContent=`${data.weather[0].main}`;
-				document.getElementById("city-m").innerHTML=`${Math.round(data.main.temp_max)}<sup>&#9898;</sup>c / ${Math.round(data.main.temp_min)}<sup>&#9898;</sup>c`;	
+				document.getElementById("city-m").innerHTML=`${Math.round(data.main.temp_max)}<sup>o</sup>c / ${Math.round(data.main.temp_min)}<sup>o</sup>c`;	
 				console.log(data);
 				console.log(status);
 			},
